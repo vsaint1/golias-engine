b4481707757183cb58a7c2d27a984c050b9c0535
vsaint1 <vsantos1.dev@gmail.com>
2025-03-25 05:00:13 -0300

    transform2d rotation

--- engine/private/core/component/transform.cpp
+++ engine/private/core/component/transform.cpp
@@ -5,7 +5,7 @@ glm::mat4 Transform::GetModelMatrix() const {
 
     glm::mat4 model = glm::mat4(1.0f);
     model           = glm::translate(model, position);
-    model           = glm::rotate(model, glm::radians(rotation.z), glm::vec3(1.0f, 0.0f, 0.0f));
+    model           = glm::rotate(model, glm::radians(rotation.z), glm::vec3(0.0f, 0.0f, 1.0f));
     model           = glm::scale(model, scale);
     return model;
 }
