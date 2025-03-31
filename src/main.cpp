b3608e00ec2ca0c20d2c92693a68901c6d46a491
vsaint1 <vsantos1.dev@gmail.com>
2025-03-31 03:01:00 -0300

    sample project

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 int SCREEN_WIDTH  = 1280;
 int SCREEN_HEIGHT = 720;
 
+Font mine_font;
 
 SDL_AppResult SDL_AppInit(void** app_state, int argc, char** argv) {
 
@@ -15,6 +16,8 @@ SDL_AppResult SDL_AppInit(void** app_state, int argc, char** argv) {
     if (!InitAudio()) {
         return SDL_APP_FAILURE;
     }
+
+    mine_font = LoadFont("fonts/Minecraft.ttf", 16);
 
     LOG_INFO("Device Name %s", SystemInfo::GetDeviceName().c_str());
     LOG_INFO("Device Model %s", SystemInfo::GetDeviceModel().c_str());
@@ -35,6 +38,13 @@ SDL_AppResult SDL_AppIterate(void* app_state) {
 
     BeginDrawing();
 
+    DrawText(mine_font, TextFormat("Hello %s", SystemInfo::GetDeviceName().c_str()),
+             {
+                 glm::vec3(20.0f, 100.f, 0.f),
+                 glm::vec3(0.f),
+                 glm::vec3(1.f),
+             },
+             {255, 255, 255, 255}, 0.0f);
 
     EndDrawing();
 
@@ -57,6 +67,8 @@ SDL_AppResult SDL_AppEvent(void* app_state, SDL_Event* event) {
 }
 
 void SDL_AppQuit(void* app_state, SDL_AppResult result) {
+
+    UnloadTexture(mine_font.texture);
 
     CloseAudio();
 
