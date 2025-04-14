21b1ea1b0e669a3d9475be3dcc8e3beaa9726dc8
vsaint1 <vsantos1.dev@gmail.com>
2025-04-14 00:04:56 -0300

    version

--- engine/public/imports.h
+++ engine/public/imports.h
@@ -41,5 +41,7 @@ const std::filesystem::path BASE_PATH = SDL_GetBasePath();
 #endif
 
 #define ENGINE_NAME        "EMBER_ENGINE"
-#define ENGINE_VERSION_STR "0.0.1"
+#define ENGINE_VERSION_STR "0.0.9"
+
+
 
