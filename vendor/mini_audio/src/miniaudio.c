5f0abffaee40d6571d280726bf9842542e2de6e0
vsaint1 <vsantos1.dev@gmail.com>
2025-05-08 20:27:49 -0300

    removed MA output logs

--- vendor/mini_audio/src/miniaudio.c
+++ vendor/mini_audio/src/miniaudio.c
@@ -1,5 +1,5 @@
 #define MINIAUDIO_IMPLEMENTATION
-#define MA_DEBUG_OUTPUT
+// #define MA_DEBUG_OUTPUT
 #define MA_NO_RESOURCE_MANAGER
 #define MA_NO_CUSTOM 
 // https://miniaud.io/docs/manual/index.html#Definitions
