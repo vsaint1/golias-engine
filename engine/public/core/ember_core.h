083e85dc2846f3778d9afa0d52bf30b63649455d
vsaint1 <vsantos1.dev@gmail.com>
2025-05-10 00:39:56 -0300

    engine/renderer refactor and code structure

--- engine/public/core/ember_core.h
+++ engine/public/core/ember_core.h
@@ -19,26 +19,8 @@
 #include "core/ember_utils.h"
 #include "core/system_info.h"
 
-/*!
 
-   @brief Backend `OpenGL` or `Metal`
-   - Choose the graphics backend
-    Type: OpenGL
-    > WEB: WebGL 3.0
-    >
 
- * * DESKTOP: OpenGL 3.3
-    > MOBILE: OpenGLES 3.0
-
-    Type: Metal
-    > Apple Devices only
-
-   @version 0.0.1
-
-*/
-enum RendererType { OPENGL, METAL };
-
-namespace Engine {
 
 /*!
     @brief Renderer struct
@@ -444,87 +426,7 @@ struct Renderer {
 };
 
 
-/*!
-    @brief Create a renderer instance
-    - Backend `OpenGL` or `Metal`
-
-    @param window SDL window instance
-
- *
- * @param view_width Viewport width
-    @param view_height Viewport height
-    @param type Renderer type `OPENGL` or
- *
- * `METAL`
-
-    @version 0.0.1
-    @return Renderer
-*/
-Renderer* CreateRenderer(SDL_Window* window, int view_width, int view_height, RendererType type);
-
-/*!
-    @brief Create a renderer instance internally
-    - Backend `OpenGL`
-
-    @param window SDL window instance
-
- *
- * @param view_width Viewport width
-    @param view_height Viewport height
-
-    @version 0.0.2
-    @return Renderer
-*/
-Renderer* CreateRendererGL(SDL_Window* window, int view_width, int view_height);
-
-/*
-    @brief Create a renderer instance internally
-    - Backend `Metal`
-*/
-Renderer* CreateRendererMTL(SDL_Window* window, int view_width, int view_height);
-
-/*!
-    @brief Get the renderer instance
-
-    @version 0.0.1
-    @return Renderer
-*/
-Renderer* GetRenderer();
-
-/*!
-    @brief Deinitialize Window, Renderer and modules
-
-    @version 0.0.1
-    @return void
-*/
-void Shutdown();
 
 
-/*!
-    @brief Initialize SDL window, renderer and modules
-    - Window
-    - Graphics Backend  `OpenGL` or `Metal`
- -
 
- * * Audio
-    - Font
 
-    @version 0.0.1
-    @param title Window title
-    @param width Window width
-    @param height
-
- * * Window height
-    @param type Renderer type `OPENGL` or `METAL`
-    @param flags SDL window flags
-
-    @link
- *
- * https://wiki.libsdl.org/SDL_WindowFlags @endlink
-
-    @return bool - true on `success` or false on `failure`
-*/
-bool Initialize(const char* title, int width, int height, RendererType type, Uint64 flags = 0);
-
-static Renderer* renderer = nullptr;
-};
