bf493c3ba16307502b5b1a1824f009250ca36f7c
vsaint1 <vsantos1.dev@gmail.com>
2025-06-10 22:41:39 -0300

    mesh indices and type update

--- engine/public/core/renderer/mesh.h
+++ engine/public/core/renderer/mesh.h
@@ -22,7 +22,7 @@ class Mesh {
 public:
     Mesh();
 
-    explicit Mesh(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices = {});
+    explicit Mesh(const std::vector<Vertex>& vertices, const std::vector<Uint32>& indices = {});
 
     ~Mesh();
 
@@ -30,14 +30,14 @@ public:
 
     void Draw(unsigned int mode = GL_TRIANGLES) const;
 
-    void Update(const std::vector<Vertex>& new_vertices);
+    void Update(const std::vector<Vertex>& new_vertices, const std::vector<Uint32>& new_indices = {});
 
     size_t GetVertexCount() const;
 
 private:
     unsigned int VAO, VBO, EBO;
     std::vector<Vertex> vertices;
-    std::vector<unsigned int> indices;
+    std::vector<Uint32> indices;
 
     void Setup();
 
