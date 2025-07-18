9238db18cb22761d6b88a5d2c6ffe15470077ee8
vsantos1 <vsantos1.dev@gmail.com>
2025-07-18 02:03:25 -0300

    drawing circle shapes

---
+++ engine/private/core/component/circle.cpp
@@ -0,0 +1,24 @@
+#include "core/component/circle.h"
+
+#include "core/ember_core.h"
+
+
+void Circle2D::ready() {
+    Node2D::ready();
+}
+
+void Circle2D::process(double delta_time) {
+    Node2D::process(delta_time);
+}
+
+void Circle2D::draw(Renderer* renderer) {
+    const auto center = get_global_transform().position;
+
+    if (_bFilled) {
+        renderer->draw_circle_filled(glm::vec3(center, 1.f), _radius, _color, _segments);
+    } else {
+        renderer->draw_circle(glm::vec3(center, 1.f), _radius, _color, _segments);
+    }
+
+    Node2D::draw(renderer);
+}
---
+++ engine/public/core/component/circle.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "core/component/node.h"
+
+
+class Circle2D final : public Node2D {
+
+public:
+    float _radius = 10.0f;
+    int _segments = 32;
+    Color _color  = Color::WHITE;
+    bool _bFilled = true;
+
+    explicit Circle2D(float radius = 10.0f, bool fill = false, Color color = Color::WHITE)
+        : _radius(radius), _color(color), _bFilled(fill) {
+    }
+
+    void ready() override;
+
+    void process(double delta_time) override;
+
+    void draw(Renderer* renderer) override;
+};
