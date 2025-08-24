6150ab0a112f9621063d5673a1f15c75d148b0aa
vsantos1 <vsantos1.dev@gmail.com>
2025-08-24 02:20:37 -0300

    feat: support more http methods and requests (non/blocking)

--- engine/private/core/http/http_client.cpp
+++ engine/private/core/http/http_client.cpp
@@ -1,31 +1,51 @@
 #include "core/http/http_client.h"
 
-size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
-    std::string* s = (std::string*)userdata;
+
+static size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
+    std::string* s = static_cast<std::string*>(userdata);
     s->append(ptr, size * nmemb);
     return size * nmemb;
 }
 
+#if defined(SDL_PLATFORM_EMSCRIPTEN)
+#include <emscripten/fetch.h>
+#endif
+
 
 HttpClient::HttpClient() {
+#if !defined(SDL_PLATFORM_EMSCRIPTEN)
     _curl = curl_easy_init();
+#endif
 }
 
 HttpClient::~HttpClient() {
-    curl_easy_cleanup(_curl);
-    _curl = nullptr;
+#if !defined(SDL_PLATFORM_EMSCRIPTEN)
+    if (_curl) {
+        curl_easy_cleanup(_curl);
+        _curl = nullptr;
+    }
+#endif
 }
 
 HttpResponse HttpClient::request(const HttpRequest& request) const {
-
     HttpResponse res;
 
+#if defined(SDL_PLATFORM_EMSCRIPTEN)
+    LOG_ERROR("Sync. HTTP/s not supported on Web builds.");
+#else
     if (_curl) {
         curl_easy_setopt(_curl, CURLOPT_URL, request.url);
 
-        if (request.method == "POST") {
+        if (request.method == "GET") {
+            curl_easy_setopt(_curl, CURLOPT_HTTPGET, 1L);
+        } else if (request.method == "POST") {
             curl_easy_setopt(_curl, CURLOPT_POST, 1L);
             curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, request.body.c_str());
+        } else {
+            curl_easy_setopt(_curl, CURLOPT_CUSTOMREQUEST, request.method.c_str());
+            if (!request.body.empty()) {
+                curl_easy_setopt(_curl, CURLOPT_POSTFIELDS, request.body.c_str());
+            }
         }
 
         curl_slist* headers = nullptr;
@@ -34,28 +54,96 @@ HttpResponse HttpClient::request(const HttpRequest& request) const {
         }
 
         curl_easy_setopt(_curl, CURLOPT_HTTPHEADER, headers);
-        curl_easy_setopt(_curl, CURLOPT_SSL_VERIFYPEER, 0L);
-        curl_easy_setopt(_curl, CURLOPT_SSL_VERIFYHOST, 0L);
 
         std::string response_data;
         curl_easy_setopt(_curl, CURLOPT_WRITEFUNCTION, write_callback);
-
         curl_easy_setopt(_curl, CURLOPT_WRITEDATA, &response_data);
 
+        curl_easy_setopt(_curl, CURLOPT_SSL_VERIFYHOST, 0L);
+        curl_easy_setopt(_curl, CURLOPT_SSL_VERIFYPEER, 0L);
+
         CURLcode code = curl_easy_perform(_curl);
-        if(code == CURLE_OK) {
+        if (code == CURLE_OK) {
             long status = 0;
             curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &status);
             res.status_code = static_cast<int>(status);
-            res.body = response_data;
-        }else {
+            res.body        = response_data;
+        } else {
             LOG_ERROR("HttpRequest Error %s", curl_easy_strerror(code));
         }
 
         curl_slist_free_all(headers);
-        curl_easy_cleanup(_curl);
+    }
+#endif
+    return res;
+}
+
+void HttpClient::request_async(const HttpRequest& request, const std::function<void(HttpResponse)>& callback) const {
+#if defined(SDL_PLATFORM_EMSCRIPTEN)
+    emscripten_fetch_attr_t attr;
+    emscripten_fetch_attr_init(&attr);
+
+    strcpy(attr.requestMethod, request.method.c_str());
+
+    if (!request.body.empty()) {
+        attr.requestData     = request.body.c_str();
+        attr.requestDataSize = request.body.size();
     }
 
+    std::vector<const char*> headers;
+    for (auto& h : request.headers) {
+        std::string kv = h.first + ": " + h.second;
+        headers.push_back(strdup(kv.c_str()));
+    }
+    headers.push_back(nullptr);
+    attr.requestHeaders = headers.data();
 
-    return res;
+    attr.attributes = EMSCRIPTEN_FETCH_LOAD_TO_MEMORY;
+
+    auto* cbPtr   = new std::function<void(HttpResponse)>(callback);
+    attr.userData = cbPtr;
+
+    attr.onsuccess = +[](emscripten_fetch_t* fetch) {
+        HttpResponse res;
+        res.status_code = fetch->status;
+        res.body.assign(fetch->data, fetch->numBytes);
+
+        auto cb = (std::function<void(HttpResponse)>*) fetch->userData;
+        (*cb)(res);
+        delete cb;
+
+        emscripten_fetch_close(fetch);
+    };
+
+    attr.onerror = +[](emscripten_fetch_t* fetch) {
+        LOG_ERROR("HttpRequest failed (emscripten)");
+        HttpResponse res;
+        res.status_code = fetch->status;
+
+        auto cb = (std::function<void(HttpResponse)>*) fetch->userData;
+        (*cb)(res);
+        delete cb;
+
+        emscripten_fetch_close(fetch);
+    };
+
+    emscripten_fetch(&attr, request.url);
+
+    for (auto h : headers) {
+        free((void*) h);
+    }
+
+#else
+    // TODO: we should use a thread pool, simpler for now...
+    const auto fut = std::async(std::launch::async, [this, request, callback]() {
+        try {
+            const HttpResponse res = this->request(request);
+            callback(res);
+        } catch (const std::exception& e) {
+            LOG_ERROR("HttpRequest async exception: %s", e.what());
+        }
+    });
+
+    (void) fut;
+#endif
 }
--- engine/public/core/http/http_client.h
+++ engine/public/core/http/http_client.h
@@ -1,15 +1,14 @@
 #pragma once
 #include "helpers/logging.h"
 #include <curl/curl.h>
-
+#include <future>
 
 struct HttpRequest {
-    const char* url = ""; ///< URL to send the request to.
+    const char* url    = ""; ///< URL to send the request to.
     std::string method = "GET"; ///< HTTP method (GET, POST, PUT, PATCH, DELETE).
     std::map<std::string, std::string> headers;
     std::string body; ///< Request body for POST/PUT requests.
 };
-
 
 struct HttpResponse {
     int status_code = 0; ///< HTTP status code.
@@ -17,14 +16,34 @@ struct HttpResponse {
     std::map<std::string, std::string> headers; ///< Response headers.
 };
 
-
+/*!
+ * @brief Simple HTTP client
+ *
+ *
+ * @details
+ * - Uses libcurl for HTTP requests (not supported on Emscripten/WebAssembly).
+ * - Supports GET, POST, PUT, DELETE methods.
+ * - Allows setting custom headers and request body.
+ * - Provides both blocking and non-blocking (async) request methods.
+ *
+ * @version 1.2.0
+ *
+ * @note
+ *  - Synchronous requests are not supported on Emscripten.
+ *
+ *  @return HttpResponse
+ *
+ */
 class HttpClient {
 public:
     HttpClient();
     ~HttpClient();
 
+    /// Blocking (not supported on Emscripten)
+    [[nodiscard]] HttpResponse request(const HttpRequest& request) const;
 
-    HttpResponse request(const HttpRequest& request) const;
+    /// Non-blocking (cross-platform)
+    void request_async(const HttpRequest& request, const std::function<void(HttpResponse)>& callback) const;
 
 private:
     CURL* _curl = nullptr;
