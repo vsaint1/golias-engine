2c8ddf948708563804885718ec739b6d82f347c0
vsantos1 <vsantos1.dev@gmail.com>
2025-08-28 23:30:33 -0300

    chore: removed useless `gl_mipmap` gen

--- engine/private/core/renderer/opengl/ember_gl.cpp
+++ engine/private/core/renderer/opengl/ember_gl.cpp
@@ -453,11 +453,12 @@ std::shared_ptr<Texture> OpenglRenderer::load_texture(const std::string& file_pa
 
     glBindTexture(GL_TEXTURE_2D, texture->id);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture->width, texture->height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_NEAREST);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
     if (!has_error_texture) {
         LOG_INFO(R"(Loaded texture with ID: %d, path: %s
