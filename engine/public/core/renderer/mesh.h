94b0b0e2e0931dda3d39924d22ed5a9564e5ac45
vsaint1 <vsantos1.dev@gmail.com>
2025-09-21 13:09:19 -0300

    chore: removed old code `mesh`

--- engine/public/core/renderer/mesh.h
+++ engine/public/core/renderer/mesh.h
@@ -1,49 +1,13 @@
 #pragma once
 
-#include "../systems/logging_sys.h"
+#include "core/systems/logging_sys.h"
 
 /*! @brief Vertex struct
 
     @version 0.0.3
 */
 struct Vertex {
-    glm::vec2 position;
-    glm::vec2 tex_coord;
-    glm::vec4 color;
+    glm::vec2 position; /// Position
+    glm::vec2 tex_coord; /// Texture coordinates (UV)
+    glm::vec4 color; /// Vertex color
 };
-
-
-/*! @brief Mesh class
-    - A mesh is a collection of vertices and indices
-    - VAO, VBO and EBO related to the mesh
-    - Material (SOON)
-
-    @deprecated
-    @version 0.0.3
-*/
-class Mesh {
-public:
-    Mesh();
-
-    explicit Mesh(const std::vector<Vertex>& vertices, const std::vector<Uint32>& indices = {});
-
-    ~Mesh();
-
-    void Bind() const;
-
-    void Draw(unsigned int mode = GL_TRIANGLES) const;
-
-    void Update(const std::vector<Vertex>& new_vertices, const std::vector<Uint32>& new_indices = {});
-
-    size_t GetVertexCount() const;
-
-private:
-    unsigned int VAO, VBO, EBO;
-    std::vector<Vertex> vertices;
-    std::vector<Uint32> indices;
-
-    void Setup();
-
-    bool bIsDirty = false;
-};
-
