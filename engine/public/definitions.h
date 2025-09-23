8c560a7c3ebc08c94a07e6dfc083d14235935a3a
vsaint1 <vsantos1.dev@gmail.com>
2025-09-23 00:01:08 -0300

    logging and pch

--- engine/public/definitions.h
+++ engine/public/definitions.h
@@ -1,27 +1,3 @@
-#include <SDL3/SDL.h>
-#include <SDL3_image/SDL_image.h>
-#include <SDL3_ttf/SDL_ttf.h>
-#include <chrono>
-#include <stdio.h>
 
-#include <lua.hpp>
-
-#include <flecs.h>
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <functional>
-#include <mutex>
-#include <nlohmann/json.hpp>
-#include <map>
-
-
-#if defined(SDL_PLATFORM_EMSCRIPTEN)
-#include <emscripten.h>
-#endif
-
-
-using Json = nlohmann::json;
-
-/// Required SDL_main for portability 
-#include <SDL3/SDL_main.h>
+#define ENGINE_NAME        "EMBER_ENGINE"
+#define ENGINE_VERSION_STR "0.0.1"
