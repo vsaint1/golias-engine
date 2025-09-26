0eb756a7d7e6e63ea270d3dfa88ed689645c7fa2
vsaint1 <vsantos1.dev@gmail.com>
2025-09-25 22:47:43 -0300

    main example

--- client/main.cpp
+++ client/main.cpp
@@ -4,6 +4,8 @@
 #define WINDOW_W 1280
 #define WINDOW_H 720
 
+
+
 int main(int argc, char* argv[]) {
 
     if (!GEngine->initialize(WINDOW_W, WINDOW_H, "nullptr")) {
@@ -11,29 +13,27 @@ int main(int argc, char* argv[]) {
     }
 
 
-    // TODO: We should get the entities from scene file (just test for now)
-    auto player = GEngine->get_world()
-                      .entity("Player")
+    auto& world = GEngine->get_world();
+
+    auto scene1 = world.entity("MenuScene").add<Scene>().add<ActiveScene>();
+    auto scene2 = world.entity("GameScene").add<Scene>();
+
+    auto player = world.entity("Player")
                       .set<Transform2D>({{100, 100}, {1, 1}, 50})
                       .set<Shape>({ShapeType::RECTANGLE, {0, 1, 0, 1}, true, {50, 50}})
-                      .set<Script>({"scripts/test.lua"});
+                      .set<Script>({"scripts/test.lua"})
+                      .child_of(scene1);
 
-
-    auto enemy = GEngine->get_world()
-                     .entity("Enemy")
+    auto enemy = world.entity("Enemy")
                      .set<Transform2D>({{300, 200}, {1, 1}, 0})
                      .set<Label2D>({"Enemy 🔥", {-5, -20}, {1, 1, 1, 1}})
-                     .set<Shape>({ShapeType::CIRCLE, {1, 0, 0, 1}, false, {32, 32}, 20});
+                     .set<Shape>({ShapeType::CIRCLE, {1, 0, 0, 1}, false, {32, 32}, 20})
+                     .child_of(scene2);
+
+    // FileAccess("user://scenes/test.json",ModeFlags::WRITE).store_string(world.to_json().c_str());
 
 
-    LOG_INFO("Player %s", player.to_json().c_str());
-
-    // LOG_INFO("World %s",GEngine->get_world().to_json().c_str());
-
-    // FileAccess scene("user://scenes/test_scene.json", ModeFlags::WRITE);
-    // scene.store_string(GEngine->get_world().to_json().c_str());
     GEngine->run();
-
 
     return 0;
 }
