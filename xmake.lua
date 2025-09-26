6ad56f80f05ea240d38d3bfa8a902894df67bf85
vsaint1 <vsantos1.dev@gmail.com>
2025-09-26 00:09:01 -0300

    tinyxml lib dependency

--- xmake.lua
+++ xmake.lua
@@ -12,8 +12,7 @@ add_requires("libsdl3 3.2.22","libsdl3_image 3.2.0",{configs = {shared = true}})
 add_requires("flecs v4.1.1", {configs = {shared = false}})
 add_requires("nlohmann_json v3.12.0", {configs = {shared = false}})
 add_requires("glm 1.0.1", {configs = {shared = false}})
-add_requires("miniaudio 0.11.23", {configs = {shared = false}})
-
+add_requires("miniaudio 0.11.23","tinyxml2 11.0.0", {configs = {shared = false}})
 
 if not is_plat("wasm") or not is_plat("android")  or not is_plat("iphoneos") then
     add_requires("doctest v2.4.9", {configs = {shared = false}})
@@ -26,7 +25,7 @@ target("engine")
     add_files("engine/private/**/*.cpp")
     add_includedirs("engine/public", {public = true}) 
     set_pcheader("stdafx.h","engine/private/stdafx.cpp")
-    add_packages("libsdl3", "libsdl3_ttf", "libsdl3_image", "lua", "flecs", "nlohmann_json","glm", "miniaudio",{public = true})
+    add_packages("libsdl3", "libsdl3_ttf", "libsdl3_image", "lua", "flecs", "nlohmann_json","glm", "miniaudio", "tinyxml2",{public = true})
 
 target("client")
     set_kind("binary")
