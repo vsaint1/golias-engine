04b27a514ae7e8e1a4b1aa9053f107fd48ec08a8
vsaint1 <vsantos1.dev@gmail.com>
2025-09-27 06:44:06 -0300

    tests: project config, engine and bindings

--- engine/public/core/project_config.h
+++ engine/public/core/project_config.h
@@ -80,7 +80,7 @@ struct Environment {
 };
 
 struct Application {
-    const char* name         = "Window - Ember Engine";
+    const char* name         = "Ember Engine - Window";
     const char* version      = "1.0";
     const char* package_name = "com.ember.engine.app"; // identifier
     const char* icon_path    = "res/icon.png";
--- tests/test_lua_binding.cpp
+++ tests/test_lua_binding.cpp
@@ -1,7 +1,7 @@
 #include <doctest/doctest.h>
 #include "core/binding/lua.h"
 
-TEST_CASE("Engine Lua Binding") {
+TEST_CASE("Engine Lua Binding - Call Function Error Handling") {
     Transform2D transform;
     transform.position = {200, 300};
     transform.scale    = {1, 1};
@@ -12,17 +12,24 @@ TEST_CASE("Engine Lua Binding") {
 
     generate_bindings(L);
 
-    lua_pushlightuserdata(L, &transform);
+    auto* ptr = (Transform2D*)lua_newuserdata(L, sizeof(Transform2D));
+    *ptr = transform;
+    luaL_setmetatable(L, "Transform2D");
     lua_setglobal(L, "transform");
 
     luaL_dostring(L, R"(
         local speed = 50
         function ready()
-            transform.position.x = speed
-            transform.position.y = speed  + 20
+            transform.position.x = transform.position.x - speed
+            transform.position.y = transform.position.y - speed
         end
     )");
 
     lua_getglobal(L, "ready");
-    REQUIRE(lua_pcall(L, 0, 0, 0) == LUA_OK);
+    REQUIRE(lua_pcall(L, 0, 0, 0) == LUA_ERRRUN);
+
+    CHECK(ptr->position.x == 200);
+    CHECK(ptr->position.y == 300);
+
+    lua_close(L);
 }
