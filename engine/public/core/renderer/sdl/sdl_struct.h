69a22056b932229a0a9f96e694bc166ad5617217
vsaint1 <vsantos1.dev@gmail.com>
2025-10-15 01:06:46 -0300

    removed sdl_font

--- engine/public/core/renderer/sdl/sdl_struct.h
+++ engine/public/core/renderer/sdl/sdl_struct.h
@@ -13,32 +13,6 @@
     @version 0.0.1
 
 */
-class SDLFont : public Font {
-
-public:
-    SDLFont(TTF_Font* f) : _font(f) {
-    }
-
-    ~SDLFont() override;
-
-    glm::vec2 get_size(const std::string& text) override;
-
-    TTF_Font* get_font() const;
-
-private:
-    TTF_Font* _font = nullptr;
-};
-
-/*!
-
-    @file sdl_struct.h
-    @brief SDL specific structures for Font and Texture.
-   
-    This file contains the definitions of SDLFont and SDLTexture classes, which are concrete implementations of the Font and Texture interfaces using the SDL library.
-
-    @version 0.0.1
-
-*/
 class SDLTexture : public Texture {
 public:
     SDLTexture(SDL_Texture* tex) : _texture(tex) {
