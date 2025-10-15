74a2b052b5f6f75967d5ce49ba6d8c7d20121ba8
vsaint1 <vsantos1.dev@gmail.com>
2025-10-15 04:02:37 -0300

    base runtime

---
+++ runtime/main.cpp
@@ -0,0 +1,15 @@
+#include "core/engine.h"
+#include <SDL3/SDL_main.h>
+
+int main(int argc, char* argv[]) {
+    const int WINDOW_W = 1280, WINDOW_H = 720;
+
+    if (!GEngine->initialize(WINDOW_W, WINDOW_H)) {
+        LOG_INFO("Failed to initialize engine");
+        return -1;
+    }
+
+        GEngine->run();
+
+        return 0;
+    }
