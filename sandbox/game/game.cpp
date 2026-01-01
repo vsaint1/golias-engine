85213cb9c7ad9b479f305fd3a1036db1ba8a0e04
vsaint1 <vsantos1.dev@gmail.com>
2026-01-01 20:36:00 -0300

    feat: sample `main` scene example

--- sandbox/game/game.cpp
+++ sandbox/game/game.cpp
@@ -1,269 +1,96 @@
 #include "game.h"
 
-#include "scene/3d/camera_component.h"
-#include "scene/3d/fp_controller_component.h"
-#include "scene/3d/mesh_component.h"
 
+std::shared_ptr<golias::Material> mat = nullptr;
+
+void SpawnBox(const glm::vec3& position) {
+
+    auto boxExtents = glm::vec3(1.f);
+    auto mesh       = golias::Mesh::CreateBox(boxExtents);
+
+    auto obj = golias::Engine::GetInstance().GetScene()->CreateObject("Box");
+
+    obj->SetPosition(position);
+
+    obj->AddComponent(new golias::MeshComponent(mesh, mat));
+
+    auto collider = std::make_shared<golias::BoxCollider>(boxExtents);
+    auto body     = std::make_shared<golias::RigidBody>(golias::EBodyType::DYNAMIC, collider, 0.0f);
+
+    // body->SetFriction(0.6f);
+    // body->SetRestitution(0.0f);
+    // body->SetDragging(0.1f);
+    // body->SetAngularDragging(0.05f);
+
+    obj->AddComponent(new golias::PhysicsComponent(body));
+}
+
+
+void SandboxApplication::RegisterTypes() {
+    Player::Register();
+}
 
 bool SandboxApplication::Initialize() {
 
-    scene = new golias::Scene();
+    std::srand((unsigned) std::time(nullptr));
+
+#if defined(SCENE_LOAD_FROM_FILE)
+
+    auto Tscene = golias::Scene::Load("scenes/main.gscene");
+    auto material = golias::Material::Load("materials/brick.mat");
+    mat = material;
+    // auto avocado = golias::GameObject::LoadModel("models/avocado/Avocado.gltf",Tscene.get());
+    // avocado->SetPosition({-2.0f, 2.0f, -2.0f});
+    // avocado->SetScale({25.0f, 25.0f, 25.0f});
+
+    golias::Engine::GetInstance().SetScene(Tscene);
+#else
+    auto& fs = golias::Engine::GetInstance().GetFileSystem();
+
+    auto rd = golias::Engine::GetInstance().GetRenderingDevice();
 
 
+    auto mesh = golias::Mesh::CreateBox();
+
+    auto scene = std::make_shared<golias::Scene>();
     golias::Engine::GetInstance().SetScene(scene);
 
-    auto fs = golias::Engine::GetInstance().GetFileSystem();
+    auto player = scene->CreateObject<Player>("Player");
+    player->Start();
+
+    auto monkey = golias::GameObject::LoadModel("models/suzanne/Suzanne.gltf", scene.get());
+    monkey->SetPosition({0.0f, 2.0f, -2.0f});
+
+    auto avocado = golias::GameObject::LoadModel("models/avocado/Avocado.gltf", scene.get());
+    avocado->SetPosition({-2.0f, 2.0f, -2.0f});
+    avocado->SetScale({25.0f, 25.0f, 25.0f});
+
+    auto torus = golias::GameObject::LoadModel("models/torus.obj", scene.get());
+    torus->SetPosition({5.0f, 2.0f, -2.0f});
+
+    mat = material;
+
+    auto godette = golias::GameObject::LoadModel("models/godette/godette.gltf", scene.get());
+    godette->SetPosition({-5.0f, 2.0f, -6.0f});
+
+    auto nagonford = golias::GameObject::LoadModel("models/nagonford/Nagonford_Animated.glb", scene.get());
+    nagonford->SetPosition({0.0f, 2.0f, -6.0f});
+
+    auto ground = scene->CreateObject("Ground");
+    ground->SetPosition({0.0f, 0.0f, -1.0f});
+
+    auto groundExtents = glm::vec3(50.0f, 2.0f, 50.0f);
+    auto groundMesh    = golias::Mesh::CreateBox(groundExtents);
+    ground->AddComponent(new golias::MeshComponent(groundMesh, material));
+
+    auto groundCollider = std::make_shared<golias::BoxCollider>(groundExtents);
+
+    auto groundBody = std::make_shared<golias::RigidBody>(golias::EBodyType::STATIC, groundCollider);
+
+    ground->AddComponent(new golias::PhysicsComponent(groundBody));
 
 
-    auto rd      = golias::Engine::GetInstance().GetRenderingDevice();
-
-    auto material = golias::Material::Load("materials/brick.mat");
-
-
-
-    std::vector<float> box_vertices = {
-        // ----- Front -----
-        -0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-        -0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-
-        // ----- Back -----
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-        -0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-
-        // ----- Left -----
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-        -0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        -0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-        -0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-
-        // ----- Right -----
-        0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-        0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-
-        // ----- Top -----
-        -0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-        0.5f,
-        0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-        0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        -0.5f,
-        0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-
-        // ----- Bottom -----
-        -0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        0,
-        0,
-        0.5f,
-        -0.5f,
-        -0.5f,
-        1,
-        1,
-        1,
-        1,
-        0,
-        0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        1,
-        1,
-        -0.5f,
-        -0.5f,
-        0.5f,
-        1,
-        1,
-        1,
-        0,
-        1,
-    };
-
-    std::vector<uint32_t> box_indices = {
-        0,  1,  2,  2,  3,  0, // front
-        4,  5,  6,  6,  7,  4, // back
-        8,  9,  10, 10, 11, 8, // left
-        12, 13, 14, 14, 15, 12, // right
-        16, 17, 18, 18, 19, 16, // top
-        20, 21, 22, 22, 23, 20 // bottom
-    };
-
-
-    golias::VertexLayout layout;
-    layout.elements = {{0, 3, EDataType::FLOAT, false, 0},
-                       {1, 3, EDataType::FLOAT, false, 3 * sizeof(float)},
-                       {2, 2, EDataType::FLOAT, false, 6 * sizeof(float)}};
-
-    layout.stride = 8 * sizeof(float);
-
-    auto mesh = rd->CreateMeshFromData(layout, box_vertices, box_indices);
-
-
-    auto camera = scene->CreateObject<golias::GameObject>("Camera");
-    camera->AddComponent(new golias::CameraComponent());
-    camera->AddComponent(new golias::FirstPersonControllerComponent());
-    camera->SetPosition({0.0f, 0.0f, 5.0f});
-    scene->SetMainCamera(camera);
-
-
-    const int grid_size = 2;
-    for (int x = -grid_size / 2; x < grid_size / 2; ++x) {
-        for (int y = -grid_size / 2; y < grid_size / 2; ++y) {
-            auto obj = scene->CreateObject();
-            obj->SetPosition({static_cast<float>(x), static_cast<float>(y), 0.0f});
-            auto meshComp = new golias::MeshComponent();
-            obj->AddComponent(new golias::MeshComponent(mesh, material));
-        }
-    }
-
+#endif
 
     spdlog::info("GameApplication Initialized successfully.");
     return true;
@@ -272,11 +99,20 @@ bool SandboxApplication::Initialize() {
 void SandboxApplication::Update(float deltaTime) {
     auto& input = golias::Engine::GetInstance().GetInputManager();
 
-    if (input.IsActionJustPressed("Jump")) {
-        spdlog::info("Jump action just pressed!");
+    if (input.IsKeyJustPressed(SDLK_E)) {
+
+        float x = (std::rand() % 10 - 5);
+        float z = (std::rand() % 10 - 5);
+
+        SpawnBox({x, 8.0f, z});
+
+        spdlog::info("Spawned box at {}, {}, {}", x, 8.0f, z);
     }
 
-    scene->Update(deltaTime);
+
+    if (auto scene = golias::Engine::GetInstance().GetScene()) {
+        scene->Update(deltaTime);
+    }
 }
 
 void SandboxApplication::Destroy() {
--- sandbox/game/game.h
+++ sandbox/game/game.h
@@ -1,14 +1,18 @@
 #include "core/application.h"
-#include "test_obj.h"
+#include "player_obj.h"
+
+
+#define SCENE_LOAD_FROM_FILE 1
 
 class SandboxApplication : public golias::Application {
 public:
+    void RegisterTypes() override;
+
     bool Initialize() override;
 
     void Update(float deltaTime) override;
 
     void Destroy() override;
 
-private:
-    golias::Scene* scene = nullptr;
+
 };
