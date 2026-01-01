bb2e772383bd52c250bf7dafc2320e670dc417bf
vsaint1 <vsantos1.dev@gmail.com>
2026-01-01 20:34:59 -0300

    improved default light

--- engine/private/core/graphics/rendering_canvas.cpp
+++ engine/private/core/graphics/rendering_canvas.cpp
@@ -18,17 +18,17 @@ namespace golias {
             shader->SetUniform("VIEW_MATRIX", camera.viewMatrix);
             shader->SetUniform("PROJECTION_MATRIX", camera.projectionMatrix);
             shader->SetUniform("VIEW_POSITION", camera.position);
-            shader->SetUniform("LIGHT_POSITION", glm::vec3(0.0f, 5.0f, 0.0f));
-            shader->SetUniform("LIGHT_COLOR", glm::vec3(1.0f, 0.95f, 0.9f)); 
-            shader->SetUniform("AMBIENT_STRENGTH", 0.25f); 
-            // shader->SetUniform("SPECULAR_STRENGTH", 0.4f);
-            // shader->SetUniform("SHININESS", 32.0f);
+            shader->SetUniform("LIGHT_POSITION", glm::vec3(5.0f, 5.0f, 5.0f));
+            shader->SetUniform("LIGHT_COLOR", glm::vec3(1.0f, 1.0f, 1.0f));
+            shader->SetUniform("AMBIENT_STRENGTH", 0.3f);
+            shader->SetUniform("SPECULAR_STRENGTH", 0.5f);
+            shader->SetUniform("SHININESS", 32.0f);
 
             rd->BindMesh(command.mesh);
             rd->DrawMesh(command.mesh);
         }
 
-        
+
         command_queue.clear();
     }
 }; // namespace golias
