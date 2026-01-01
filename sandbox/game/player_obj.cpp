58a55c50a4ae5630bd5d977d9b1ff27a9aaadc7f
vsaint1 <vsantos1.dev@gmail.com>
2026-01-01 20:36:28 -0300

    feat: example player obj

--- sandbox/game/player_obj.cpp
+++ sandbox/game/player_obj.cpp
@@ -2,11 +2,7 @@
 
 void Player::Start() {
 
-    spdlog::info("Player created.");
-}
-
-Player::Player() {
-
+#if defined(SCENE_LOAD_FROM_FILE)
     auto scene = golias::Engine::GetInstance().GetScene();
 
     fpControllerComp = new golias::FirstPersonControllerComponent();
@@ -16,24 +12,43 @@ Player::Player() {
     camera->SetPosition({0.0f, 2.0f, 5.0f});
     scene->SetMainCamera(camera);
 
-    auto gun = golias::GameObject::LoadModel("models/carbine/scene.gltf");
+    auto gun = golias::GameObject::LoadModel("models/carbine/scene.gltf",scene);
     gun->SetParent(camera);
     gun->SetPosition({0.75f, -0.5f, -0.75f});
     gun->SetScale({-1.0f, 1.0f, 1.0f});
 
-    if (auto anim = gun->GetComponent<golias::AnimationComponent>()) {
-
-
-        if (auto bullet = gun->FindChildByName("bullet_33")) {
-            bullet->SetActive(false);
-        }
-
-        if (auto fire = gun->FindChildByName("BOOM_35")) {
-            fire->SetActive(false);
-        }
-
-        animComp = anim;
+    if (auto bullet = gun->FindChildByName("bullet_33")) {
+        bullet->SetActive(false);
     }
+
+    if (auto fire = gun->FindChildByName("BOOM_35")) {
+        fire->SetActive(false);
+    }
+
+    if (auto ac = gun->GetComponent<golias::AnimationComponent>()) {
+        animComp = ac;
+    }
+
+#else
+
+  if (auto bullet = FindChildByName("bullet_33")) {
+        bullet->SetActive(false);
+    }
+
+    if (auto fire =  FindChildByName("BOOM_35")) {
+        fire->SetActive(false);
+    }
+
+    if (auto gun = FindChildByName("Gun")) {
+        if (auto ac = gun->GetComponent<golias::AnimationComponent>()) {
+            animComp = ac;
+        }
+    }
+
+#endif
+}
+
+Player::Player() {
 }
 
 void Player::Update(float deltaTime) {
